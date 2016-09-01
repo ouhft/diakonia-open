6358c05ad57745224cd1a7182e7aced162b3854b
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2016-09-01 13:23:19 +0100

    Tweak to hide generated files from git

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diakonia.eyeaux',
+    'diakonia.fhirstation',
 ]
 
 MIDDLEWARE = [
